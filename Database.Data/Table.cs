8ddd7e7c17c420578e5986fffc49e85eacdc2c24
Jordan Kelwick <jordankelwick@gmail.com>
2018-08-23 23:14:05 +0100

    WIP

---
+++ Database.Data/Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Database.Data
+{
+    public class Table<TRow>
+    {
+        public string Name { get; set; }
+        public IQueryable<TRow>[] Rows { get; set; }
+    }
+}
